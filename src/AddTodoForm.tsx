398416dc73b11494f480a317f1c3cf5ee1835319
Paulo Jerônimo <paulojeronimo@gmail.com>
2020-12-27 09:42:35 -0300

    AddTodoForm handleSubmit

--- src/AddTodoForm.tsx
+++ src/AddTodoForm.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useState } from 'react'
+import React, { ChangeEvent, FormEvent, useState } from 'react'
 
 export const AddTodoForm: React.FC = () => {
     const [newTodo, setNewTodo] = useState("")
@@ -7,10 +7,14 @@ export const AddTodoForm: React.FC = () => {
         setNewTodo(e.target.value)
     }
 
+    const handleSubmit = (e: FormEvent<HTMLButtonElement>) => {
+        e.preventDefault()
+    }
+
     return (
         <form>
             <input type="text" onChange={handleChange}/>
-            <button type="submit">Add Todo</button>
+            <button type="submit" onClick={handleSubmit}>Add Todo</button>
         </form>
     )
 }
