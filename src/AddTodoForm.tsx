52947f7c7fae7093d5b8551e1a06261a3232a5ff
Paulo Jerônimo <paulojeronimo@gmail.com>
2020-12-27 09:48:51 -0300

    AddTodoForm AddTodoFormProps

--- src/AddTodoForm.tsx
+++ src/AddTodoForm.tsx
@@ -1,6 +1,10 @@
 import React, { ChangeEvent, FormEvent, useState } from 'react'
 
-export const AddTodoForm: React.FC = () => {
+interface AddTodoFormProps {
+    addTodo: (newTodo: string) => void
+}
+
+export const AddTodoForm: React.FC<AddTodoFormProps> = ({addTodo}) => {
     const [newTodo, setNewTodo] = useState("")
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -9,6 +13,7 @@ export const AddTodoForm: React.FC = () => {
 
     const handleSubmit = (e: FormEvent<HTMLButtonElement>) => {
         e.preventDefault()
+        addTodo(newTodo)
     }
 
     return (
