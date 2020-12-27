72ca55f1078ec101dd7caccdbea83e7fa82c46fe
Paulo Jerônimo <paulojeronimo@gmail.com>
2020-12-27 09:34:44 -0300

    AddTodoForm handleChange

--- src/AddTodoForm.tsx
+++ src/AddTodoForm.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { ChangeEvent, useState } from 'react'
 
-export const AddTodoForm: React.FC = () =>
-    <form>
-        <input type="text"/>
-        <button type="submit">Add Todo</button>
-    </form>
+export const AddTodoForm: React.FC = () => {
+    const [newTodo, setNewTodo] = useState("")
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setNewTodo(e.target.value)
+    }
+
+    return (
+        <form>
+            <input type="text" onChange={handleChange}/>
+            <button type="submit">Add Todo</button>
+        </form>
+    )
+}
